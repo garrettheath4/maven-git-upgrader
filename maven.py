929489ffb9fea8fc64c2ebf073e2b95c875fc712
Garrett Koller <gkoller@catalist.us>
2020-04-26 18:21:04 -0400

    Maven module is now able to set new versions of dependencies
    settings versions through properties is not yet tested

--- maven.py
+++ maven.py
@@ -5,6 +5,7 @@ Pom: represents a Maven pom.xml file that can be modified
 import re
 import xml.etree.ElementTree as ET
 import logging
+from typing import List, Optional
 
 from cxml import CommentedTreeBuilder
 
@@ -14,6 +15,7 @@ log = logging.getLogger("maven")
 
 nsp = 'm'
 nsu = "http://maven.apache.org/POM/4.0.0"
+NSM = f"{{{nsu}}}"
 NS = {nsp: nsu}
 M = nsp + ':'
 ET.register_namespace('', nsu)
@@ -21,12 +23,19 @@ ET.register_namespace('', nsu)
 
 class Dependency:
     def __init__(self, dependency_xml: ET.Element, properties_xml: ET.Element):
-        self.group = dependency_xml.find(M+"groupId", NS).text
-        self.artifact = dependency_xml.find(M+"artifactId", NS).text
-        self.version_xml = dependency_xml.find(M+"version", NS)
+        group_xml = dependency_xml.find(M+"groupId", NS)
+        if group_xml is None:
+            log.warning("Dependency group XML should not be None!")
+        self.group: str = group_xml.text
+        self.artifact: str = dependency_xml.find(M+"artifactId", NS).text
+        self.version_xml: Optional[ET.Element] = \
+            dependency_xml.find(M+"version", NS)
         if self.version_xml.text.startswith("${"):
-            prop_name = re.findall(r'\$\{([^}]+)\}', self.version_xml.text)[0][0]
+            prop_name = re.findall(r'\${([^}]+)}', self.version_xml.text)[0]
             self.version_xml = properties_xml.find(M+prop_name, NS)
+
+    def set_version(self, version_number: str):
+        self.version_xml.text = str(version_number)
 
 
 class Pom:
@@ -34,16 +43,14 @@ class Pom:
         parser = ET.XMLParser(target=CommentedTreeBuilder())
         self.xml_tree = ET.parse(filename, parser)
         self.project = self.xml_tree.getroot()
-        log.debug(self.project)
-        self.dependencies_xml = self.project.find(M+"dependencies", NS)
-        log.debug(self.dependencies_xml)
-        self.properties_xml = self.project.find(M+"properties", NS)
-        self.dependencies = map(lambda d: Dependency(d, self.properties_xml),
-                                self.dependencies_xml)
+        self._dependencies_xml = self.project.find(M+"dependencies", NS)
+        self._properties_xml = self.project.find(M+"properties", NS)
+        self.dependencies: List[Dependency] = list(map(
+            lambda d: Dependency(d, self._properties_xml),
+            filter(lambda x: x.tag == NSM+"dependency", self._dependencies_xml)))
 
     def save(self, filename: str):
         xml_str = ET.tostring(self.project, encoding='unicode')
-        log.debug(type(xml_str))
         project_tag_too_wide = \
             '<project xmlns="http://maven.apache.org/POM/4.0.0"' \
             ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' \
@@ -55,10 +62,7 @@ class Pom:
             '         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0' \
             ' http://maven.apache.org/maven-v4_0_0.xsd">'
         if xml_str.startswith(project_tag_too_wide):
-            log.debug("startswith == true")
             xml_str = xml_str.replace(project_tag_too_wide, project_tag_two_lines, 1)
-        else:
-            log.debug("startswith == false")
         if not xml_str.endswith('\n'):
             xml_str += '\n'
         log.debug(xml_str[0:220])
--- test.py
+++ test.py
@@ -14,6 +14,7 @@ class TestMaven(unittest.TestCase):
         input_filename = "pom-unittest-in.xml"
         output_filename = "pom-unittest-out.xml"
         pom = Pom(input_filename)
+        self.assertGreater(len(pom.dependencies), 0)
         pom.save(output_filename)
         self.assertTrue(os.path.isfile(output_filename))
         with open(input_filename, 'r') as i, open(output_filename, 'r') as o:
@@ -25,6 +26,41 @@ class TestMaven(unittest.TestCase):
                 o_curr = o.readline()
                 num_lines += 1
                 self.assertEqual(i_curr, o_curr)
+            log.debug("number of lines checked: " + str(num_lines))
+            self.assertGreater(num_lines, 1)
+
+    def test_set_version(self):
+        input_filename = "pom-unittest-in.xml"
+        output_filename = "pom-unittest-out.xml"
+        new_version = "9.9.9"
+        pom = Pom(input_filename)
+        self.assertGreater(len(pom.dependencies), 0)
+        artifact = "scalatest_2.11"
+        artifact_tag = f"<artifactId>{artifact}</artifactId>"
+        scalatest_dep_list = list(filter(
+            lambda d: d.artifact == artifact, pom.dependencies))
+        self.assertEqual(len(scalatest_dep_list), 1)
+        scalatest_dep_list[0].set_version(new_version)
+        pom.save(output_filename)
+        self.assertTrue(os.path.isfile(output_filename))
+        with open(input_filename, 'r') as i, open(output_filename, 'r') as o:
+            i_curr = "\n"
+            o_curr = i_curr
+            next_line_is_updated_version = False
+            num_lines = 0
+            while i_curr and o_curr:
+                i_curr = i.readline()
+                o_curr = o.readline()
+                num_lines += 1
+                if next_line_is_updated_version:
+                    self.assertIn(new_version, o_curr)
+                    self.assertNotIn(new_version, i_curr)
+                    self.assertNotEqual(i_curr, o_curr)
+                    next_line_is_updated_version = False
+                else:
+                    if artifact_tag in o_curr:
+                        next_line_is_updated_version = True
+                    self.assertEqual(i_curr, o_curr)
             log.debug("number of lines checked: " + str(num_lines))
             self.assertGreater(num_lines, 1)
 
