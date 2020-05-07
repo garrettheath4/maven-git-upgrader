b72bb5fef91916fa80500af8dc0866d251671eed
Garrett Koller <gkoller@catalist.us>
2020-05-07 00:33:44 -0400

    Moved package version and docstring from __main__.py to __init__.py

--- mavengitupgrader/__main__.py
+++ mavengitupgrader/__main__.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-Checks for updates to dependencies in a Maven project and creates a Git
-branch for each update for isolated testing and easy integration.
-"""
-
-__author__ = "Garrett Heath Koller"
-__copyright__ = "Copyright 2020, Garrett Heath Koller"
-__license__ = "MIT"
-__version__ = "1.0.1"
-__maintainer__ = "Garrett Heath Koller"
-__email__ = "garrettheath4@gmail.com"
-__status__ = "Stable"
-
 import argparse
 import logging
 import os
